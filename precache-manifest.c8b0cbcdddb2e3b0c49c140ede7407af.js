b39d1844e26b7c8be874a443f72dd3398eabc85b
稀饭 <xifan233@163.com>
2021-06-16 02:40:03 +0800

    🎉 重新打包

---
+++ precache-manifest.c8b0cbcdddb2e3b0c49c140ede7407af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df86b3ae1e0d895da27a",
+    "url": "css/app.b8122c79.css"
+  },
+  {
+    "revision": "97d7d15af675ce8b5216",
+    "url": "css/chunk-vendors.bf3a68b0.css"
+  },
+  {
+    "revision": "0b05e423b40dfb71b0b47bf586577728",
+    "url": "index.html"
+  },
+  {
+    "revision": "df86b3ae1e0d895da27a",
+    "url": "js/app.814938f7.js"
+  },
+  {
+    "revision": "97d7d15af675ce8b5216",
+    "url": "js/chunk-vendors.3a3b8b36.js"
+  },
+  {
+    "revision": "81b37924d1a6708473073ec18139fa41",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
